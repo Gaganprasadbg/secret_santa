503aaf2f1b39ecd1f50b783fb73ba8fb2eddd186
Lokesh b s <130569078+Gaganprasadbg@users.noreply.github.com>
2023-12-17 10:17:23 +0530

    seceret
    seceret

--- src/components/Header/headerindex.tsx
+++ src/components/Header/headerindex.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
+import ForestIcon from '@mui/icons-material/Forest';
+
 
 const Header = () => {
   return (
     <div className=' ' style={{backgroundColor:"rgba(255,0,0, 0.5)"}}>
       <div className='max-w-[1200px] mx-auto flex justify-between items-center h-[4rem] text-white px-4' >
         <div className='text-white'>
-            Gagan
+            <ForestIcon sx={{fontSize:"2rem"}}/>
         </div>
         <div>
-            <a href="/">Axios</a>
+          <button className=''>
+          Click Here
+          </button>
+            
         </div>
       </div>
     </div>
